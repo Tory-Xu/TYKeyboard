d28b1f27695158436a5d8d5bbcfd1e209e39e5fc
Tory Xu <756165690@qq.com>
2020-11-24 11:07:26 +0800

    创建键盘，获取键盘响应者

--- TYKeyboard/TYKeyboard.swift
+++ TYKeyboard/TYKeyboard.swift
@@ -7,7 +7,25 @@
 
 import UIKit
 
+extension UIResponder {
+    static var currentFirstResponder: AnyObject?
+    @objc private class func ty_currentFirstResponder() -> AnyObject {
+        UIApplication.shared.sendAction(#selector(ty_currentFirstResponder), to: nil, from: nil, for: nil) as AnyObject
+    }
+    
+    @objc private func ty_findFirstResonder(sender: AnyObject) -> Void {
+        UIResponder.currentFirstResponder = sender
+    }
+}
+
 class TYKeyboard: UIInputView {
 
+    var keyInput: AnyObject? {
+        get {
+            return UIResponder.currentFirstResponder
+        }
+    }
+    
+    
     
 }
